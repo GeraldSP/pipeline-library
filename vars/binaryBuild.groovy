9171e90e69ef5193fafcbd767b0b929d25b33cc2
David Espinosa <despinos@redhat.com>
2021-02-09 19:40:10 +0000

    recommendation of log-level by esj

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -22,14 +22,19 @@ def call(BinaryBuildInput input) {
     assert input.buildFromFlag?.trim()   : "Param buildFromFlag should be defined."
     assert input.buildFromPath?.trim()   : "Param buildFromPath should be defined."
 
-    openshift.loglevel(input.loglevel)
+    openshift.loglevel("debug")
 
     openshift.withCluster(input.clusterAPI, input.clusterToken) {
         openshift.withProject(input.projectName) {
             echo "Attemping to start and follow 'buildconfig/${input.buildConfigName}' in ${openshift.project()}"
 
             def buildConfig = openshift.selector('bc', input.buildConfigName)
+
+            echo "\n\n Build config: ${buildConfig} \n\n"
+
             def build       = buildConfig.startBuild("${input.buildFromFlag}=${input.buildFromPath}", '--wait')
+
+	    
             build.logs('-f')
         }
     }
